5ba8e3c5af3173efe52b3234b3577d9edfb8bac9
Shea <sheae123@gmail.com>
2026-08-07 15:19:38 -0700

    oled display functionality

--- serial/main.py
+++ serial/main.py
@@ -1,10 +1,24 @@
 # from dac import DAC
 # from synth import Synth
 from buttons import *
-
+import oled
 
 # dac = DAC()
 
 # synth = Synth(dac)
 
 # synth.note_on(440)  # Play A4 note (440 Hz)
+
+# Menu options
+oled.init()
+
+oled.clear()
+
+oled.text("HELLO", 10, 5)
+
+oled.text("> PIANO", 10, 20)
+oled.text("  BASS", 10, 32)
+oled.text("  SYNTH", 10, 44)
+
+oled.show()
+
