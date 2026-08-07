6390134d710cc54fba851f1d18f33e5486aebf4e
Richie Rojas <richierojas26@gmail.com>
2026-08-07 12:12:16 -0700

    Add sine waveform generation to synth

--- serial/synth.py
+++ serial/synth.py
@@ -1,5 +1,7 @@
 import math
 
+SAMPLE_RATE = 44100  # Sample rate in Hz
+
 class Synth:
 
     def __init__(self, dac):
@@ -8,19 +10,20 @@ class Synth:
         self.current_note = None
         self.waveform = 'sine'  # Default waveform type
 
-    def sine_wave(self, frequency, time):
-        return math.sin(2 * math.pi * frequency * time)
+    def sine_wave(self, frequency, sample_number):
+        t = sample_number / SAMPLE_RATE
+        return math.sin(2 * math.pi * frequency * t)
 
     def note_on(self, frequency):
         self.current_note = frequency
         print ("Playing:", frequency)
 
-        #temporary sample generation for demonstration purposes
-        t = 0
-        sample = self.sine_wave(frequency, t)
+        sample_number = 0  # Initialize sample number for waveform generation
+        sample = self.sine_wave(frequency, sample_number)
         #send the sample to the DAC with volume adjustment
         self.dac.write(sample * self.volume)
 
     def note_off(self):
         self.current_note = None
+        self.dac.write(0)  # Stop the sound by sending zero to the DAC
         print("Note off")
