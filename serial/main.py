20415dbfbf563f6f3ccadba07e97a527fe498e65
Richie Rojas <richierojas26@gmail.com>
2026-08-07 22:37:33 -0700

    Add JSON hardware events

--- serial/main.py
+++ serial/main.py
@@ -1,32 +1,49 @@
 import buttons 
 import oled 
 import time
-import oled
 import menu
 import joystick
 import slider
+import json
 
 last_move = 0
+last_volume = -1
 
 oled.init()
 
 while True:
 
     # Update volume from slider
-    menu.volume = slider.percent()
+    current_volume = slider.percent()
 
-    # Draw menu
+    if current_volume != last_volume:
+        menu.volume = current_volume
+
+        print(json.dumps({
+            "type": "volume",
+            "value": current_volume,
+        }))
+
+        last_volume = current_volume
+    # Draw the menu 
     menu.draw()
-
     now = time.monotonic()
 
     # Navigate menu
     if joystick.up() and now - last_move > 0.20:
         menu.move_up()
+        print(json.dumps({
+            "type": "menu",
+            "value": menu.current(),
+        }))
         last_move = now
 
     elif joystick.down() and now - last_move > 0.20:
         menu.move_down()
+        print(json.dumps({
+            "type": "menu",
+            "value": menu.current(),
+        }))
         last_move = now
 
 # Use for volume control with joystick
@@ -42,23 +59,33 @@ while True:
     # Select instrument
     if joystick.pressed():
 
-        print("Selected:", menu.current())
-        print("Volume:", menu.volume)
-
+        print(json.dumps({
+            "type": "select",
+            "value": menu.current(),
+        }))
         while joystick.pressed():
             pass
 
         time.sleep(0.1)
-        
+    #Read button events
     events = buttons.update()
 
     for event in events:
 
         if event[0] == "pressed":
-            print("PLAY", event[1])
+
+            print(json.dumps({
+                "type": "button",
+                "value": event[1],
+            }))
 
         elif event[0] == "released":
-            print("STOP", event[1], "Held:", round(event[2], 2))
+
+            print(json.dumps({
+                "type": "button_released",
+                "value": event[1],
+                "held": event[2],
+            }))
 
 
 
