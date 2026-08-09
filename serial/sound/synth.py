4ecd07a3e0df763f6f0f9be10ba13f85fe18e681
rylander925 <rylander925@gmail.com>
2026-08-08 19:37:51 -0700

    Added pausing/resuming synth

--- serial/sound/synth.py
+++ serial/sound/synth.py
@@ -3,8 +3,8 @@ import time
 import asyncio
 import json
 
-from scale import Scale
-from synth_presets import Synth_Presets
+from sound.scale import Scale
+from sound.synth_presets import Synth_Presets
 
 #To add:
 #pitch bend
@@ -24,11 +24,15 @@ class Synth_Wrapper:
         self.preset = Synth_Presets(preset)
         self.synth = synthio.Synthesizer(sample_rate=self.sample_rate, waveform=self.preset.waveform, envelope=self.preset.envelope)
         #? Remove buffering? self.notes = [synthio.Note(frequency=synthio.midi_to_hz(scale.get_midi_note(i))) for i in range(1, 9)]
-        self.held_notes = []
+        self.held_notes = {}
         self.recordings = [[] for _ in range(Synth_Wrapper.RECORDING_CHANNELS)]
         self.recording_synths = [synthio.Synthesizer(sample_rate=self.sample_rate, envelope=self.preset.envelope, waveform=self.preset.waveform)
                                  for _ in range(Synth_Wrapper.RECORDING_CHANNELS)]
         self.record_start_times = [None] * Synth_Wrapper.RECORDING_CHANNELS
+        self.existing_playbacks = [None] * Synth_Wrapper.RECORDING_CHANNELS
+        self.playback_pause_events = [asyncio.Event() for _ in range(Synth_Wrapper.RECORDING_CHANNELS)]
+        for e in self.playback_pause_events:
+            e.set() #Create events to pause/start playback, set flag to allow playback on start
 
     #Returns a deep copy of an envelope object
     @staticmethod
@@ -41,13 +45,22 @@ class Synth_Wrapper:
             attack_level=base.attack_level,
             sustain_level=base.sustain_level
         )
+
+    #Returns note midi as an ID, used as the key in a dict of stored notes
+    def _get_note_key(self, note_number, accidental=0, octave_offset=0):
+        return (self.scale.get_midi_note(note_number, accidental_offset=accidental, octave_offset=octave_offset))
         
     #Play a note and buffer it for modulation
     #Note number should be 1-8 for scale notes
     #   For intervals before, -1 refers to 1 below the root
-    def press(self, note_number, accidental= 0, extra_sustain=False):
+    def press(self, note_number, accidental= 0, octave_offset=0, extra_sustain=False):
+        note_key = self._get_note_key(note_number, accidental=accidental, octave_offset=octave_offset)
+
+        if note_key in self.held_notes:
+            self.release(note_number=note_number, accidental=accidental, octave_offset=octave_offset)
+
         #!warn serial logging may cause slowdowns if lots of notes, add separate function for chords
-        print(f'"event": "note-played", "value": "{self.scale}-{note_number}"')
+        #print(f'"event": "note-played", "value": "{self.scale}-{note_number}"')
 
         note = synthio.Note(frequency=synthio.midi_to_hz(self.scale.get_midi_note(note_number, accidental_offset=accidental)),
                             envelope=self.preset.envelope,
@@ -60,8 +73,9 @@ class Synth_Wrapper:
             note.envelope = Synth_Wrapper.copyEnvelope(note.envelope)
             note.envelope.sustain_level = min(1.5*sustain, 1)
             note.envelope.release_time *= 0.66
-        if sustain > 0.0:
-            self.held_notes.append(note)
+
+        #Buffer note for its eventual release
+        self.held_notes[note_key] = note
 
         #self.held_notes.append(note)
         self.log_event("note-pressed", note)
@@ -69,43 +83,67 @@ class Synth_Wrapper:
         return note
 
     #Release a note from the active buffer
-    def release(self, note):
+    def release(self, note_number, accidental = 0):
+        note_key = self._get_note_key(note_number=note_number, accidental=accidental)
+        note = self.held_notes.pop(note_key, None)
         #Broadcast relase of note
-        self.log_event("note-released", note)
-        try:
-            self.held_notes.remove(note)
-        except ValueError:
-            print('Attempted to remove note not marked as held')
+        if note is None:
+            print("Attempted to release a key that is not held")
+            return
 
+        self.log_event("note-released", note)
         self.synth.release(note)
 
     #Record currently played notes and modulations
-    def record(self, record_channel_index):
+    def record(self, record_channel_index=0):
         self.recordings[record_channel_index].clear()
         self.record_start_times[record_channel_index] = time.monotonic()
+        print("Synth started recording")
         #TODO: Broadcast recording start for OLED to display
         #Keep track of time, and time of:
             #note presses,
             #modulations,
             #note releases
     
+    
     #Ends recording. Sets recorded notes held when recording ends to be released at the end of playback 
-    def end_record(self, record_channel_index):
-        for note in self.held_notes:
-                    self.log_event("note-released", note)
+    def end_record(self, record_channel_index=0):
+        print("Synth ended recording")
+        for note in self.held_notes.values():
+                self.log_event("note-released", note)
         self.record_start_times[record_channel_index] = None
 
         #TODO: Broadcast recording pause for OLED to display
+
+    def start_playback(self, record_channel_index=0):
+        existing = self.existing_playbacks[record_channel_index]
+        if existing is not None and not existing.done():
+            #TODO: Allow play to restart recording
+            print("Attempted to replay existing recording")
+            return
+        asyncio.create_task(self.play_record(record_channel_index=record_channel_index))
+
+    def pause_playback(self, record_channel_index=0):
+        self.playback_pause_events[record_channel_index].clear()
+
+    def resume_playback(self, record_channel_index=0):
+        self.playback_pause_events[record_channel_index].set()
+
     
-    async def play_record(self, record_channel_index):
+    async def play_record(self, record_channel_index=0):
+
+        print("Synth playing record")
+        pause_event = self.playback_pause_events[record_channel_index]
         #TODO: Complete play record
         #? Remove envelope and waveform?
-        prev_elapsed_time = 0
-        for event in self.recordings[record_channel_index]:
-            delay = event['time'] - prev_elapsed_time
-            prev_elapsed_time = event['time']
-            await asyncio.sleep(delay)
-            await self.handle_record_event(event, self.recording_synths[record_channel_index])
+        while True:
+            prev_elapsed_time = 0
+            for event in self.recordings[record_channel_index]:
+                delay = event['time'] - prev_elapsed_time
+                prev_elapsed_time = event['time']
+                await asyncio.sleep(delay)
+                await pause_event.wait()
+                await self.handle_record_event(event, self.recording_synths[record_channel_index])
 
         #wait for some time
     
