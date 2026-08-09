e09bc5fd4a999d653cd1c00044e95c68d241ba28
Richie Rojas <richierojas26@gmail.com>
2026-08-08 20:04:18 -0700

    style hero section

---
+++ websockets/client/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+export function Hero() {
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                
+                <p className="hero-tag">
+                    J² BATTLE OF THE BUILDS
+                </p>
+
+                <h1> COSMIC SPELLSYNTH</h1>
+
+                <h2> A musical experience out of this world </h2>
+
+                <p className="hero-description">
+                    Join us for an unforgettable night of music, lights, and cosmic vibes. 
+                    Experience the magic of our spellbinding performance that will transport you to another dimension. Get ready to dance under the stars and let the music take you on a journey through the cosmos.
+                </p>
+
+                <a href="#live" className="hero-button">
+                    Experience the Magic
+                </a>
+            </div>
+        </section>
+    )
+}   
